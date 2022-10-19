e32fba7a2cbae15a51008502c21a15fdc702dae4
hewl3 <hewl3@lenovo.com>
2022-10-19 16:10:57 +0800

    mybatis

---
+++ snow-parent/snow-login/src/main/java/cn/snowBoard/mapper/testMapper.java
@@ -0,0 +1,8 @@
+package cn.snowBoard.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface testMapper extends BaseMapper {
+}
