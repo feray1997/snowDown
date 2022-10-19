ecd2e46270d203c544f443dabb44df795a17ece7
hewl3 <hewl3@lenovo.com>
2022-10-19 17:48:24 +0800

    nacos配置

--- snow-parent/snow-gateway/src/main/java/cn/snowBoard/SnowGateWayApplication.java
+++ snow-parent/snow-gateway/src/main/java/cn/snowBoard/SnowGateWayApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SnowGateWayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SnowGateWayApplication.class,args);
